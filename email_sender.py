19de32c7ba88fbdf3b11e0cb249d13e07d9a8e5e
gatraenggar <gatraenggarwicaksono@gmail.com>
2021-10-02 17:41:31 +0800

    handled token expiration

--- email_sender.py
+++ email_sender.py
@@ -2,45 +2,49 @@ from main.token_manager import TokenManager
 from decouple import config
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import smtplib, ssl, sys, uuid
+import smtplib, ssl, sys
 
 sys.path.append("..")
 from token_manager import TokenManager
 
 def send_email(payload_string, routing_key):
-    recipient_email = payload_string.split()[0]
-    auth_token = payload_string.split()[1]
+    try:
+        recipient_email = payload_string.split()[0]
+        auth_token = payload_string.split()[1]
 
-    tokenPayload = TokenManager.verify_random_token(auth_token)
+        tokenPayload = TokenManager.verify_random_token(auth_token)
 
-    services = {
-        "email_confirmation": {
-            "subject": "Email Verification for Registration",
-            "template": "./templates/email_confirmation.html",
-            "uri": tokenPayload["uri"] + auth_token,
-        },
-        "workspace_invitation": {
-            "subject": "Workspace Invitation by One of Your Friend",
-            "template": "./templates/workspace_invitation.html",
-            "uri": tokenPayload["uri"] + auth_token,
+        services = {
+            "email_confirmation": {
+                "subject": "Email Verification for Registration",
+                "template": "./templates/email_confirmation.html",
+                "uri": tokenPayload["uri"] + auth_token,
+            },
+            "workspace_invitation": {
+                "subject": "Workspace Invitation by One of Your Friend",
+                "template": "./templates/workspace_invitation.html",
+                "uri": tokenPayload["uri"] + auth_token,
+            }
         }
-    }
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = services[routing_key]["subject"]
-    message["From"] = config("EMAIL_HOST_USER")
-    message["To"] = recipient_email
+        message = MIMEMultipart("alternative")
+        message["Subject"] = services[routing_key]["subject"]
+        message["From"] = config("EMAIL_HOST_USER")
+        message["To"] = recipient_email
 
-    html_file = open(services[routing_key]["template"], 'r', encoding='utf-8')
-    source_code = html_file.read().replace("{{url}}", config("API_URL") + services[routing_key]["uri"])
+        html_file = open(services[routing_key]["template"], 'r', encoding='utf-8')
+        source_code = html_file.read().replace("{{url}}", config("API_URL") + services[routing_key]["uri"])
 
-    template = MIMEText(source_code, "html")
+        template = MIMEText(source_code, "html")
 
-    message.attach(template)
+        message.attach(template)
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(config("EMAIL_HOST_USER"), config("EMAIL_HOST_PASSWORD"))
-        server.sendmail(
-            message["From"], message["To"], message.as_string()
-        )
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(config("EMAIL_HOST_USER"), config("EMAIL_HOST_PASSWORD"))
+            server.sendmail(
+                message["From"], message["To"], message.as_string()
+            )
+    except Exception as e:
+        print(e)
+        return
